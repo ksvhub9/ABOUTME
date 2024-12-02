15915c7efe8a857627b57f070eb66c9dc5a20829
ksvhub9 <waponline.in@gmail.com>
2024-12-03 00:50:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Sticky Navigation Menu
+
 let nav = document.querySelector("nav");
 let scrollBtn = document.querySelector(".scroll-button a");
 document.getElementById("ksv").addEventListener("click", function (event) {
@@ -6,14 +6,14 @@ document.getElementById("ksv").addEventListener("click", function (event) {
   const contentBox = document.getElementById("content-box");
   const dimBackground = document.getElementById("dim-background");
 
-  // Set custom properties for animation
+  
   contentBox.style.setProperty("--button-left", `${button.left + button.width / 2}px`);
   contentBox.style.setProperty("--button-top", `${button.top + button.height / 2}px`);
 
-  // Show dim background
+  
   dimBackground.classList.add("active");
 
-  // Show content box with animation
+ 
   contentBox.classList.remove("hidden");
   contentBox.classList.add("show");
 });
@@ -22,7 +22,7 @@ function closeContentBox() {
   const contentBox = document.getElementById("content-box");
   const dimBackground = document.getElementById("dim-background");
 
-  // Hide content box and dim background
+  
   contentBox.classList.remove("show");
   contentBox.classList.add("hidden");
 
@@ -32,13 +32,13 @@ function closeContentBox() {
 
 
 
-// Side Navigation Menu
+
 let body = document.querySelector("body");
 let navBar = document.querySelector(".navbar");
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
 
-// Open side navigation
+
 menuBtn.onclick = function () {
   navBar.classList.add("active");
   menuBtn.style.opacity = "0";
@@ -55,10 +55,10 @@ const hideNavMenu = () => {
   scrollBtn.style.pointerEvents = "auto";
 };
 
-// Close side navigation
+
 cancelBtn.onclick = hideNavMenu;
 
-// Close side navigation when a menu link is clicked
+
 let navLinks = document.querySelectorAll(".menu li a");
 navLinks.forEach((link) => {
   link.addEventListener("click", hideNavMenu);
